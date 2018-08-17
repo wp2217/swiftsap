bf98f2de152593dab206955fc89106a32f46f3d0
RocWang 王鹏 <wp217@126.com>
2018-08-17 16:09:19 +0800

    commit

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,17 +1,31 @@
-module.exports = async(ctx, next) => {
+module.exports = async (ctx, next) => {
   const Request = require('../models/Request');
   let cates = [];
 
-  let result = await Request.getByIduser(1);
-  
-  for (const idx in result) {
-    if (cates.indexOf(result[idx].category) == -1) {
-      cates.push({
-        title: result[idx].category,
-        num: 8
-      });
+  await Request.getByIduser(1).then((result) => {
+    for (const idx in result) {
+      if (cates.indexOf(result[idx].category) == -1) {
+        cates.push({
+          title: result[idx].category,
+          num: 8
+        });
+      }
     }
-  }
+  }).catch((err) => {
+    ctx.state.data = err;
+  })
 
-  ctx.state.data = cates;
+
+  // let result = await Request.getByIduser(1);
+
+  // for (const idx in result) {
+  //   if (cates.indexOf(result[idx].category) == -1) {
+  //     cates.push({
+  //       title: result[idx].category,
+  //       num: 8
+  //     });
+  //   }
+  // }
+
+  //ctx.state.data = cates;
 }
