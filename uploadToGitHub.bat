d562390a04e393460411536ccab3c61de9d022bb
Roc Wang 王鹏 <wp217@126.com>
2022-05-29 23:34:01 +0800

    Delete uploadToGitHub.bat

--- uploadToGitHub.bat
+++
@@ -1,7 +0,0 @@
-git init
-git add .
-git commit -m "Commit"
-git remote add origin https://github.com/wp2217/swiftsap.git
-git pull origin master --allow-unrelated-histories
-git push origin master
-pause
