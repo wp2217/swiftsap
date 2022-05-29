1cc0596568ec472758e784e185d7549a1103d569
Roc Wang 王鹏 <wp217@126.com>
2022-05-29 23:33:53 +0800

    Delete uploadToGitee.bat

--- uploadToGitee.bat
+++
@@ -1,7 +0,0 @@
-git init
-git add .
-git commit -m "Commit"
-git remote add origin https://gitee.com/wp217/swiftsap.git
-git pull origin master --allow-unrelated-histories
-git push -u origin master
-pause
